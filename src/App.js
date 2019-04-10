e20972cac3efd4172ca70a23fc305fc9f4b507f5
Van Nguyen <van.nguyenthuyvi@petronas.com.my>
2019-04-11 00:45:58 +0700

    [todo ext] build basic structure

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    let myTodos = [];
+
+    return (
+      <div className="App">
+        <h2>To do List</h2>
+        <ul>
+          
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
